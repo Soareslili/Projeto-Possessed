a0ef4b171680c28bcea626e1f4de3c356d28f801
Soareslili <lidianesantossoares2@gmail.com>
2026-02-12 08:15:02 -0300

    Ataulizando tsconfing.json

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // 👈 Agora o @ aponta pra /src
+      "@": path.resolve(__dirname, "./src"), 
     },
   },
 });
