754dc51eac9460c0eaf0e081bfa53751c20c6e8b
Soareslili <lidianesantossoares2@gmail.com>
2026-02-12 07:47:15 -0300

    Realizado ajuste em AOS

--- src/components/AOSInit.tsx
+++ src/components/AOSInit.tsx
@@ -1,14 +1,17 @@
 'use client';
 
 import { useEffect } from 'react';
+import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 export default function AOSInit() {
   useEffect(() => {
-    import('aos').then((AOS) => {
-      AOS.init({ duration: 800, once: true, offset: 0 });
+    AOS.init({
+      duration: 1000,
+      once: true,
+      offset: 50,
+      easing: 'ease-in-out',
     });
   }, []);
-
   return null;
 }
